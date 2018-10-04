29dedad5591b3d5d9314f7665af9d796779bf5a5
David Winnicki <david.winnicki@ebox.ca>
2018-10-04 10:20:39 -0400

    Moved some functions to ViewModel

--- app/src/main/java/com/winnicki/rsstopstories/ui/main/MainFragment.kt
+++ app/src/main/java/com/winnicki/rsstopstories/ui/main/MainFragment.kt
@@ -14,7 +14,6 @@ import com.winnicki.rsstopstories.R
 import com.winnicki.rsstopstories.db.NewsStoriesDatabase
 import com.winnicki.rsstopstories.db.entity.NewsStory
 import com.winnicki.rsstopstories.ui.web.WebActivity
-import com.winnicki.rsstopstories.utils.RssFeedProvider
 import kotlinx.android.synthetic.main.main_fragment.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
@@ -22,8 +21,6 @@ import org.jetbrains.anko.uiThread
 class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
-
-    private var db: NewsStoriesDatabase? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -33,8 +30,12 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        db = context?.let { NewsStoriesDatabase.getInstance(it) }
         initRecyclerView()
+    }
+
+    override fun onDestroy() {
+        NewsStoriesDatabase.destroyInstace()
+        super.onDestroy()
     }
 
     private fun initRecyclerView() {
@@ -44,31 +45,31 @@ class MainFragment : Fragment() {
             addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
 
             doAsync {
-                val data = if (isNetworkAvailable()) {
-                    RssFeedProvider().parseUrl(RSS_FEED_URL, db?.newsStoryDao())
-                } else {
-                    db?.newsStoryDao()?.getAll() ?: emptyList()
-                }
-                uiThread {
-                    adapter = NewsStoryAdapter(data, object : NewsStoryAdapter.OnItemClickListener {
-                        override fun onItemClick(item: NewsStory) {
-                            startActivity(WebActivity.getStartIntent(context, item.link))
-                        }
-                    })
+                NewsStoriesDatabase.getInstance(context)?.let { db ->
+                    val data = if (isNetworkAvailable()) {
+                        viewModel.getAllNewsStories(db)
+                    } else {
+                        viewModel.getAllNewsStoriesOffline(db)
+                    }
+                    uiThread {
+                        adapter = NewsStoryAdapter(data, object : NewsStoryAdapter.OnItemClickListener {
+                            override fun onItemClick(item: NewsStory) {
+                                startActivity(WebActivity.getStartIntent(context, item.link))
+                            }
+                        })
+                    }
                 }
             }
         }
     }
 
     private fun isNetworkAvailable(): Boolean {
-        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        val activeNetworkInfo = (context?.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager).activeNetworkInfo
         return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 
     companion object {
-        const val RSS_FEED_URL = "https://www.cbc.ca/cmlink/rss-topstories"
-
         fun newInstance() = MainFragment()
     }
 }
--- app/src/main/java/com/winnicki/rsstopstories/ui/main/MainViewModel.kt
+++ app/src/main/java/com/winnicki/rsstopstories/ui/main/MainViewModel.kt
@@ -1,5 +1,18 @@
 package com.winnicki.rsstopstories.ui.main
 
 import android.arch.lifecycle.ViewModel
+import com.winnicki.rsstopstories.db.NewsStoriesDatabase
+import com.winnicki.rsstopstories.utils.RssFeedProvider
 
-class MainViewModel : ViewModel()
+class MainViewModel : ViewModel() {
+
+    fun getAllNewsStories(db: NewsStoriesDatabase) =
+            RssFeedProvider().parseUrl(RSS_FEED_URL, db)
+
+    fun getAllNewsStoriesOffline(db: NewsStoriesDatabase) =
+            db.newsStoryDao().getAll()
+
+    companion object {
+        const val RSS_FEED_URL = "https://www.cbc.ca/cmlink/rss-topstories"
+    }
+}
